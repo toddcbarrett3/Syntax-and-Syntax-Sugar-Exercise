96c087a8953df51900218e2a1f9a50caa84feccc
Todd Barrett <toddcbarrett3@gmail.com>
2023-06-22 11:28:09 -0700

    Made Code Sugary

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 11;
+            var response = "";
+
+            response = answer < 9 ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+           
+            Console.WriteLine(response);
         }
     }
 }
